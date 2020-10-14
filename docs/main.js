e84c6f3859b260c21d38feb8ec6d1f6db0978916
MuZhou233 <muzhou233@outlook.com>
2020-10-14 08:51:53 +0800

    update

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,8 @@
+config = {
+  "server_url":".",
+  "default_info":"Server disconnected"
+}
+
 function ready(fn) {
     if (document.readyState != 'loading'){
       fn();
@@ -5,6 +10,23 @@ function ready(fn) {
       document.addEventListener('DOMContentLoaded', fn);
     }
 }
+function ajax(type, url, success, error) {
+  var request = new XMLHttpRequest()
+  request.open(type, config["server_url"]+url, true)
+  request.onload = function(){
+    success(this.status,this.response)
+  }
+  request.onerror = function(){
+    error()
+  }
+  request.send()
+}
+
 ready(function(){
-  console.log("init")
+  console.log("start init")
+  ajax('GET', '/status.json', function(state, res){
+    console.log('success',state,res)
+  }, function(){
+    console.log('error')
+  })
 })
