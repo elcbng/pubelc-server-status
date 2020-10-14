343f889afe24b6864c88dc593e926a81a5b8c5aa
CerteKim <Seelkadoow@live.com>
2020-10-14 23:55:11 +0800

    tweak

--- main.go
+++ main.go
@@ -11,38 +11,38 @@ import (
 func main() {
 	r := router.NewMuxer()
 	URL := config.GetURL()
-	r.AddRoute("GET", URL+"/v1/oneshot"+"/mem", func(w http.ResponseWriter, r *http.Request) {
+	r.AddRoute("GET", URL+"/oneshot"+"/mem", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write(stat.Catter("mem")); err != nil {
 			fmt.Println("Http write failed:", err)
 		}
 	})
-	r.AddRoute("GET", URL+"/v1/oneshot"+"/cpu", func(w http.ResponseWriter, r *http.Request) {
+	r.AddRoute("GET", URL+"/oneshot"+"/cpu", func(w http.ResponseWriter, r *http.Request) {
 
 		if _, err := w.Write(stat.Catter("cpu")); err != nil {
 			fmt.Println("Http write failed:", err)
 		}
 	})
-	r.AddRoute("GET", URL+"/v1/oneshot"+"/disk", func(w http.ResponseWriter, r *http.Request) {
+	r.AddRoute("GET", URL+"/oneshot"+"/disk", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write(stat.Catter("disk")); err != nil {
 			fmt.Println("Http write failed:", err)
 		}
 	})
-	r.AddRoute("GET", URL+"/v1/oneshot"+"/uptime", func(w http.ResponseWriter, r *http.Request) {
+	r.AddRoute("GET", URL+"/oneshot"+"/uptime", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write(stat.Catter("uptime")); err != nil {
 			fmt.Println("Http write failed:", err)
 		}
 	})
-	r.AddRoute("GET", URL+"/v1/oneshot"+"/load", func(w http.ResponseWriter, r *http.Request) {
+	r.AddRoute("GET", URL+"/oneshot"+"/load", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write(stat.Catter("load")); err != nil {
 			fmt.Println("Http write failed:", err)
 		}
 	})
-	r.AddRoute("GET", URL+"/v1/oneshot"+"/network", func(w http.ResponseWriter, r *http.Request) {
+	r.AddRoute("GET", URL+"/oneshot"+"/network", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write(stat.Catter("network")); err != nil {
 			fmt.Println("Http write failed:", err)
 		}
 	})
-	r.AddRoute("GET", URL+"/v1/oneshot"+"/all", func(w http.ResponseWriter, r *http.Request) {
+	r.AddRoute("GET", URL+"/oneshot"+"/all", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write(stat.CatAll()); err != nil {
 			fmt.Println("Http write failed:", err)
 		}
